b847a5ca1a0504b3e5a753a2e11e0a5d147a0f84
Trevor Conn <yuggoth23@yahoo.com>
2015-09-09 20:26:03 -0500

    Added parsing of JSON representing market calendar information into response object model.

---
+++ TradierClient/Exchange/Responses/DTO/MarketDay.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradierClient.Exchange.Responses.DTO
+{
+    public class MarketDay
+    {
+        public DateTime Date { get; set; }
+        public bool IsOpen { get; set; }
+        public string Description { get; set; }
+        public TimeInterval PreMarket { get; set; }
+        public TimeInterval Open { get; set; }
+        public TimeInterval PostMarket { get; set; }
+    }
+
+    public struct TimeInterval
+    {
+        private string _start, _end;
+        public TimeInterval(string start, string end)
+        {
+            _start = start;
+            _end = end;
+        }
+
+        public string Start
+        {
+            get
+            {
+                return _start;
+            }
+        }
+
+        public string End
+        {
+            get
+            {
+                return _end;
+            }
+        }
+    }
+}
--- TradierClient/Exchange/Responses/GetMarketCalendarResponse.cs
+++ TradierClient/Exchange/Responses/GetMarketCalendarResponse.cs
@@ -3,11 +3,96 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace TradierClient.Exchange.Responses
 {
     public class GetMarketCalendarResponse:BaseResponse
     {
-        public GetMarketCalendarResponse(RawResponse raw) : base(raw) { }
+        public GetMarketCalendarResponse(RawResponse raw) : base(raw)
+        {
+            _days = Parse(raw.Content);
+        }
+
+        private List<DTO.MarketDay> _days;
+        public List<DTO.MarketDay> Days
+        {
+            get
+            {
+                return _days ?? (_days = new List<DTO.MarketDay>());
+            }
+        }
+
+        private List<DTO.MarketDay> Parse(string responseContent)
+        {
+            var days = new List<DTO.MarketDay>();
+
+            JObject json = JObject.Parse(responseContent);
+            JToken outer = json["calendar"]["days"].FirstOrDefault();
+            if (outer != null)
+            {
+                JArray array = null;
+                if (((JProperty)outer).Value is JArray)
+                    array = (JArray)((JProperty)outer).Value;
+                else
+                    throw new ArgumentOutOfRangeException("Expected array not found at json[\"calendar\"][\"days\"].Children()");
+
+                for (int i = 0; i < array.Count; i++)
+                {
+                    var day = new DTO.MarketDay();
+                    foreach (JToken token in array[i].Children())
+                    {
+                        var property = (JProperty)token;
+                        switch (property.Name)
+                        {
+                            case "date":
+                                day.Date = DateTime.Parse((string)property.Value);
+                                break;
+                            case "status":
+                                if (String.Compare((string)property.Value, "open", true) == 0)
+                                    day.IsOpen = true;
+                                break;
+                            case "description":
+                                day.Description = (string)property.Value;
+                                break;
+                            case "premarket":
+                                day.PreMarket = ParseInterval(token);
+                                break;
+                            case "open":
+                                day.Open = ParseInterval(token);
+                                break;
+                            case "postmarket":
+                                day.PostMarket = ParseInterval(token);
+                                break;
+                        }
+                    }
+                    days.Add(day);
+                }
+            }
+            else
+                throw new NullReferenceException("No child found at json[\"calendar\"][\"days\"]");
+
+            return days;
+        }
+
+        private static DTO.TimeInterval ParseInterval(JToken token)
+        {
+            string start = "";
+            string end = "";
+            foreach (JToken child in token.First().Children())
+            {
+                var childProperty = (JProperty)child;
+                var value = (string)childProperty.Value;
+                if (String.Compare(childProperty.Name, "start", true) == 0)
+                {
+                    start = value;
+                }
+                else if (String.Compare(childProperty.Name, "end", true) == 0)
+                {
+                    end = value;
+                }
+            }
+            return new DTO.TimeInterval(start, end);
+        }
     }
 }
